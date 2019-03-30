d38288af0f683aed417ceddbfe80104b597b0fa5
Shane ONeill <oneill.shanepatrick@gmail.com>
2019-03-30 13:07:47 -0400

    media page navbar and footer changes

--- main.js
+++ main.js
@@ -13,9 +13,9 @@
         function myFunction() {
             var navbar = document.getElementById("myNavbar");
             if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-                navbar.className = "w3-bar" + " w3-card" + " w3-animate-top" + " w3-white";
+                navbar.className = "w3-bar" + " w3-card" + " w3-animate-top" + " w3-white nav-bar-scrolled";
             } else {
-                navbar.className = navbar.className.replace(" w3-card w3-animate-top w3-white", "");
+                navbar.className = navbar.className.replace(" w3-card w3-animate-top w3-white nav-bar-scrolled", "");
             }
         }
 
